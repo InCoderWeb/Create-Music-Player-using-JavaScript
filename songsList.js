bdbcd4868446473dfce3dfbb048d0a3ed616809e
Ashutosh Tiwari <ashutoshtiwary2021@gmail.com>
2023-04-16 14:25:32 +0530

    first commit

---
+++ songsList.js
@@ -0,0 +1,37 @@
+const allSongs = [
+    {
+        id: "1",
+        name: "Falling Down",
+        artist: "Alex Skrindo",
+        image: "Images/falling down.jpg",
+        src: "music/falling down.mp3",
+    },
+    {
+        id: "2",
+        name: "Everything",
+        artist: "JJL. Malarkey",
+        image: "Images/everything.jpg",
+        src: "music/everything.mp3",
+    },
+    {
+        id: "3",
+        name: "Dreamer",
+        artist: "Alan Walker",
+        image: "Images/dreamer.jpg",
+        src: "music/dreamer.mp3",
+    },
+    {
+        id: "4",
+        name: "094",
+        artist: "JOXION",
+        image: "Images/094.jpg",
+        src: "music/094.mp3",
+    },
+    {
+        id: "5",
+        name: "Immortal",
+        artist: "CITYWLKR",
+        image: "Images/immortal.jpg",
+        src: "music/immortal.mp3",
+    },
+]
